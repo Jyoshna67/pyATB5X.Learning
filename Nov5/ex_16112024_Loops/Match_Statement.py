d93edbeb06b305a5fdd7acc55362fb443b67287f
gangalakunta.jyoshna <jyoshreddy1@gmail.com>
2024-11-18 17:57:34 +0530

    16/11/24

--- Nov5/ex_16112024_Loops/Match_Statement.py
+++ Nov5/ex_16112024_Loops/Match_Statement.py
@@ -1,15 +1,15 @@
-#It is similar like "Switch" in java.
-#Write a program to ask the user which browser he wants to run automation
+# It is similar like "Switch" in java.
+# Write a program to ask the user which browser he wants to run automation
 
-browser_Name=input("Enter Browser Name:")
-match browser_Name:
-    case "firefox" :
+browser_name = input("Enter Browser Name:")
+match browser_name:
+    case "Firefox":
 print("Starting Firefox!")
-case "chrome":
-        print("Starting Chrome!")
-        case "safari":
+case  "chrome":
+print("Starting Chrome!")
+case "safari":
 print("Starting Safari!")
 case "opera":
 print("Starting Opera!")
-case _:
+case_:
 print("Browser Not Found!")
